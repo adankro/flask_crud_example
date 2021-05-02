49ecc5ef358c08890708dc6f4078c280c303bbc5
Adan Rosales <adan.rosales@conversica.com>
2021-05-01 20:02:31 -0500

    typo estados

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask import (
 from flask_sqlalchemy import SQLAlchemy
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_file = "sqlite:///{}".format(os.path.join(project_dir, "etados.db"))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "estados.db"))
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = database_file
